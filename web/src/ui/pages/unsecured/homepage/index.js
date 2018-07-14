937e0dc19c11fb256e122b07a983cad58d1bbfa0
Romario Herbert <romarioh39@gmail.com>
2018-07-14 12:19:22 -0600

    homepage modal

--- web/src/ui/pages/unsecured/homepage/index.js
+++ web/src/ui/pages/unsecured/homepage/index.js
@@ -1,6 +1,7 @@
 import CSSModules from 'react-css-modules'
 import React, { Component } from 'react'
 import css from './index.css'
+import Modal from './modal'
 // var Carousel = require('react-responsive-carousel').Carousel
 
 // import CR1 from './Users/romarioh39/Documents/my_first_web_page/public/index.html'
@@ -10,8 +11,23 @@ import css from './index.css'
 // You can use someone else's carousel library/implementation
 // But you have to design/build the modal yourself...you can't use anyone else's modal
 
-class Modal extends Component {
-  state = { p1: 1, p2: 2, p3: 3 }
+class homepage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      isModalOpen: false,
+      p1: 1,
+      p2: 2,
+      p3: 3
+    }
+  }
+
+  toggleModal = () => {
+    console.log('state before', this.state)
+    this.setState({ isModalOpen: !this.state.isModalOpen })
+    console.log('here i am', this.state)
+  }
+
   render() {
     return (
       <div styleName="main-container">
@@ -59,11 +75,16 @@ class Modal extends Component {
             }
           }} >Right</button>
         </div>
+        <div onClick={this.toggleModal}>Open Modal</div>
+        <Modal
+          isOpen={this.state.isModalOpen}
+          toggleModal={this.toggleModal}
+        />
       </div>
     )
   }
 }
 
-export default CSSModules(Modal, css)
+export default CSSModules(homepage, css)
 
 // <img styleName="car-pix" src={require('./modal/pix/2.jpg')} alt="k" />
